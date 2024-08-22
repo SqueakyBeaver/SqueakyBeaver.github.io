5fbb1cd56ad365c76462c0aa4d2e414cb1fe208a
Beaverr <beaver@transgender.army>
2024-08-22 10:23:58 -0500

    Rewrite :3

--- nav.js
+++
@@ -1,8 +0,0 @@
-fetch('nav.html')
-.then(res => res.text())
-.then(text => {
-    let oldElem = document.querySelector("script#replace_with_navbar");
-    let newElem = document.createElement("div");
-    newElem.innerHTML = text;
-    oldElem.parentNode.replaceChild(newElem, oldElem);
-})
